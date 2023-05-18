bd3f5e3c31ad4d08bc3b7d665555048a1a77971a
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2023-05-17 23:03:26 -0500

    Add ViewModels

---
+++ Calculator/MVVM/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Calculator.MVVM.Models;
+using Microsoft.Maui.Controls;
+using System.ComponentModel;
+
+namespace  Calculator.MVVM.ViewModels
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private CalculatorModel calculator;
+
+        public CalculatorModel Calculator
+        {
+            get { return calculator; }
+            set
+            {
+                calculator = value;
+                OnPropertyChanged(nameof(Calculator));
+            }
+        }
+
+        public Command AddCommand { get; }
+        public Command SubtractCommand { get; }
+        public Command MultiplyCommand { get; }
+        public Command DivideCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            Calculator = new CalculatorModel();
+
+            AddCommand = new Command(ExecuteAddCommand);
+            SubtractCommand = new Command(ExecuteSubtractCommand);
+            MultiplyCommand = new Command(ExecuteMultiplyCommand);
+            DivideCommand = new Command(ExecuteDivideCommand);
+        }
+
+        private void ExecuteAddCommand()
+        {
+            Calculator.Add();
+        }
+
+        private void ExecuteSubtractCommand()
+        {
+            Calculator.Subtract();
+        }
+
+        private void ExecuteMultiplyCommand()
+        {
+            Calculator.Multiply();
+        }
+
+        private void ExecuteDivideCommand()
+        {
+            Calculator.Divide();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

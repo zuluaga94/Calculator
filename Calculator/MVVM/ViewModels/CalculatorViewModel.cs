4de4e7b04084d847b838c818ca66829222fa6588
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2023-05-20 14:20:18 -0500

    Add DataTable

--- Calculator/MVVM/ViewModels/CalculatorViewModel.cs
+++ Calculator/MVVM/ViewModels/CalculatorViewModel.cs
@@ -1,108 +1,75 @@
 ﻿using Calculator.MVVM.Models;
 using Microsoft.Maui.Controls;
 using System.ComponentModel;
+using System.Data;
 using System.Windows.Input;
 
-namespace  Calculator.MVVM.ViewModels
+namespace Calculator.MVVM.ViewModels
 {
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private CalculatorModel _calculator;
 
-   
-        public class CalculatorViewModel : INotifyPropertyChanged
+        private string _displayText;
+        public string DisplayText
         {
-            private CalculatorModel _calculator;
-
-            private string _displayText;
-            public string DisplayText
+            get { return _displayText; }
+            set
             {
-                get { return _displayText; }
-                set
-                {
-                    _displayText = value;
-                    OnPropertyChanged(nameof(DisplayText));
-                }
+                _displayText = value;
+                OnPropertyChanged(nameof(DisplayText));
             }
+        }
 
-            public ICommand NumberCommand { get; }
-            public ICommand OperatorCommand { get; }
-            public ICommand ClearCommand { get; }
-            public ICommand EqualCommand { get; }
+        public ICommand NumberCommand { get; }
+        public ICommand OperatorCommand { get; }
+        public ICommand ClearCommand { get; }
+        public ICommand EqualCommand { get; }
 
-            public CalculatorViewModel()
-            {
-                _calculator = new CalculatorModel();
-                NumberCommand = new Command<string>(AppendNumber);
-                OperatorCommand = new Command<string>(SetOperator);
-                ClearCommand = new Command(Clear);
-                EqualCommand = new Command(PerformCalculation);
-            }
+        public CalculatorViewModel()
+        {
+            _calculator = new CalculatorModel();
+            NumberCommand = new Command<string>(AppendNumber);
+            OperatorCommand = new Command<string>(SetOperator);
+            ClearCommand = new Command(Clear);
+            EqualCommand = new Command(PerformCalculation);
+        }
 
-            private void AppendNumber(string number)
-            {
-                DisplayText += number;
-            }
+        private void AppendNumber(string number)
+        {
+            DisplayText += number;
+        }
 
         private void SetOperator(string op)
         {
             if (!string.IsNullOrEmpty(DisplayText))
             {
-                _calculator.FirstNumber = double.Parse(DisplayText);
-                _calculator.Operator = op;
                 DisplayText += op;
             }
         }
 
-
         private void Clear()
-            {
-            //alculator.FirstNumber = null;
-            //_calculator.Operator = null;
+        {
+            _calculator.FirstNumber = 0;
+            _calculator.Operator = null;
             DisplayText = string.Empty;
-            }
+        }
 
         private void PerformCalculation()
         {
-            if (!string.IsNullOrEmpty(DisplayText) && _calculator.FirstNumber != null && !string.IsNullOrEmpty(_calculator.Operator))
+            if (!string.IsNullOrEmpty(DisplayText))
             {
-                var secondNumberText = DisplayText.Substring(DisplayText.IndexOf(_calculator.Operator, StringComparison.Ordinal) + 1);
-                _calculator.SecondNumber = double.Parse(secondNumberText);
-
-                switch (_calculator.Operator)
-                {
-                    case "+":
-                        _calculator.Result = _calculator.FirstNumber + _calculator.SecondNumber;
-                        break;
-                    case "-":
-                        _calculator.Result = _calculator.FirstNumber - _calculator.SecondNumber;
-                        break;
-                    case "*":
-                        _calculator.Result = _calculator.FirstNumber * _calculator.SecondNumber;
-                        break;
-                    case "C":
-                        _calculator.Result = 0;
-                        break;
-                    case "/":
-                        if (_calculator.SecondNumber != 0)
-                        {
-                            _calculator.Result = _calculator.FirstNumber / _calculator.SecondNumber;
-                        }
-                        else
-                        {
-                            // Manejo de la división por cero
-                            _calculator.Result = 0;
-                        }
-                        break;
-                }
-
-                DisplayText = _calculator.Result.ToString();
+                var dataTable = new DataTable();
+                var result = dataTable.Compute(DisplayText, "");
+                DisplayText = result.ToString();
             }
         }
-
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-            protected virtual void OnPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
+}

e18c5e6ae4ad1ee18252c7a793cb73ef859f09b0
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2023-05-20 13:48:32 -0500

    Correction Without Code Behind

--- Calculator/MVVM/ViewModels/CalculatorViewModel.cs
+++ Calculator/MVVM/ViewModels/CalculatorViewModel.cs
@@ -1,63 +1,108 @@
 ﻿using Calculator.MVVM.Models;
 using Microsoft.Maui.Controls;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace  Calculator.MVVM.ViewModels
 {
-    public class CalculatorViewModel : INotifyPropertyChanged
-    {
-        private CalculatorModel calculator;
 
-        public CalculatorModel Calculator
+   
+        public class CalculatorViewModel : INotifyPropertyChanged
         {
-            get { return calculator; }
-            set
+            private CalculatorModel _calculator;
+
+            private string _displayText;
+            public string DisplayText
             {
-                calculator = value;
-                OnPropertyChanged(nameof(Calculator));
+                get { return _displayText; }
+                set
+                {
+                    _displayText = value;
+                    OnPropertyChanged(nameof(DisplayText));
+                }
+            }
+
+            public ICommand NumberCommand { get; }
+            public ICommand OperatorCommand { get; }
+            public ICommand ClearCommand { get; }
+            public ICommand EqualCommand { get; }
+
+            public CalculatorViewModel()
+            {
+                _calculator = new CalculatorModel();
+                NumberCommand = new Command<string>(AppendNumber);
+                OperatorCommand = new Command<string>(SetOperator);
+                ClearCommand = new Command(Clear);
+                EqualCommand = new Command(PerformCalculation);
+            }
+
+            private void AppendNumber(string number)
+            {
+                DisplayText += number;
+            }
+
+        private void SetOperator(string op)
+        {
+            if (!string.IsNullOrEmpty(DisplayText))
+            {
+                _calculator.FirstNumber = double.Parse(DisplayText);
+                _calculator.Operator = op;
+                DisplayText += op;
             }
         }
 
-        public Command AddCommand { get; }
-        public Command SubtractCommand { get; }
-        public Command MultiplyCommand { get; }
-        public Command DivideCommand { get; }
 
-        public CalculatorViewModel()
+        private void Clear()
+            {
+            //alculator.FirstNumber = null;
+            //_calculator.Operator = null;
+            DisplayText = string.Empty;
+            }
+
+        private void PerformCalculation()
         {
-            Calculator = new CalculatorModel();
+            if (!string.IsNullOrEmpty(DisplayText) && _calculator.FirstNumber != null && !string.IsNullOrEmpty(_calculator.Operator))
+            {
+                var secondNumberText = DisplayText.Substring(DisplayText.IndexOf(_calculator.Operator, StringComparison.Ordinal) + 1);
+                _calculator.SecondNumber = double.Parse(secondNumberText);
 
-            AddCommand = new Command(ExecuteAddCommand);
-            SubtractCommand = new Command(ExecuteSubtractCommand);
-            MultiplyCommand = new Command(ExecuteMultiplyCommand);
-            DivideCommand = new Command(ExecuteDivideCommand);
+                switch (_calculator.Operator)
+                {
+                    case "+":
+                        _calculator.Result = _calculator.FirstNumber + _calculator.SecondNumber;
+                        break;
+                    case "-":
+                        _calculator.Result = _calculator.FirstNumber - _calculator.SecondNumber;
+                        break;
+                    case "*":
+                        _calculator.Result = _calculator.FirstNumber * _calculator.SecondNumber;
+                        break;
+                    case "C":
+                        _calculator.Result = 0;
+                        break;
+                    case "/":
+                        if (_calculator.SecondNumber != 0)
+                        {
+                            _calculator.Result = _calculator.FirstNumber / _calculator.SecondNumber;
+                        }
+                        else
+                        {
+                            // Manejo de la división por cero
+                            _calculator.Result = 0;
+                        }
+                        break;
+                }
+
+                DisplayText = _calculator.Result.ToString();
+            }
         }
 
-        private void ExecuteAddCommand()
-        {
-            Calculator.Add();
-        }
-
-        private void ExecuteSubtractCommand()
-        {
-            Calculator.Subtract();
-        }
-
-        private void ExecuteMultiplyCommand()
-        {
-            Calculator.Multiply();
-        }
-
-        private void ExecuteDivideCommand()
-        {
-            Calculator.Divide();
-        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
-}
